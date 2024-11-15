5c7fc90e3a680b0cca0e5063ca08292e54ef8057
giannisgkountras <gkountrasioannis@gmail.com>
2024-11-15 12:09:05 +0200

    Fixed css bug in admin panel

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -5,7 +5,7 @@ import CheckInOutUsers from "../components/CheckInOutUsers";
 
 export default function Admin() {
     return (
-        <div className="flex flex-col justify-center items-center w-full h-full">
+        <div className="flex flex-col justify-center items-center w-full h-screen">
             <TopNavigation />
             <div className="flex w-full h-full justify-evenly items-center md:flex-col md:justify-center md:pt-4 md:pb-16">
                 <RegisterUser />
